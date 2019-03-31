e8fbfa93a947e201697c697ecb374c5309f9b7b3
themother <jacktowleretc@hotmail.com>
2019-03-31 18:09:57 +0100

    more tests for update method around fixed tiles

--- src/test/scala/BoardTest.scala
+++ src/test/scala/BoardTest.scala
@@ -52,4 +52,22 @@ class BoardTest extends FlatSpec with Matchers {
     onTest.tiles(7)(4) shouldBe '3'
   }
 
+  it should "update an already updated tile" in {
+    val onTest = Board().update(1, 3, '2')
+    onTest.tiles(3)(1) shouldBe '2'
+    val onTest2 = onTest.update(1, 3, '3')
+    onTest2.tiles(3)(1) shouldBe '3'
+  }
+
+  it should "not update initial tiles" in {
+    val emptyLine = List.tabulate(9)(_ => ' ')
+    val nonEmptyLine = List.tabulate(9)(i => if (i == 7) '1' else ' ')
+    val onTest = Board(List.tabulate(9){i =>
+      if (i == 7) nonEmptyLine
+      else emptyLine})
+    onTest.tiles(7)(7) shouldBe '1'
+    val onTest2 = onTest.update(7, 7, '2')
+    onTest2.tiles(7)(7) shouldBe '1'
+  }
+
 }
